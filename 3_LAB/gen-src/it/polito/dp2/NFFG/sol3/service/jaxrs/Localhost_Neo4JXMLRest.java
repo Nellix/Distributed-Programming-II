2588fa1f5e99c01ae19c359dcdabcc9da12040da
Nellix <nellix93@gmail.com>
2017-02-18 01:05:46 +0100

    Assgnment 3
    -bug fixed

--- 3_LAB/gen-src/it/polito/dp2/NFFG/sol3/service/jaxrs/Localhost_Neo4JXMLRest.java
+++ 3_LAB/gen-src/it/polito/dp2/NFFG/sol3/service/jaxrs/Localhost_Neo4JXMLRest.java
@@ -18,7 +18,7 @@ import com.sun.jersey.api.uri.UriTemplate;
 
 @Generated(value = {
     "wadl|http://localhost:8080/Neo4JXML/rest/application.wadl"
-}, comments = "wadl2java, http://wadl.java.net", date = "2017-02-18T00:53:59.761+01:00")
+}, comments = "wadl2java, http://wadl.java.net", date = "2017-02-18T01:04:53.394+01:00")
 public class Localhost_Neo4JXMLRest {
 
     /**
